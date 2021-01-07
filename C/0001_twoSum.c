76abeccda89298e38f49098e6942a5e5b4a0e6f5
Soran_Ibrahim <53035661+RibbonsAlmark@users.noreply.github.com>
2021-01-07 18:00:38 +0800

    Update 0001_twoSum.c

--- C/0001_twoSum.c
+++ C/0001_twoSum.c
@@ -1,9 +1,10 @@
+#include <malloc.h>
 /*
 Knowledge points:
 1.Time complexity
 2.Space complexity
 3.hashtable
-4.python build in method: enumerate
+4.c build in method: malloc
 */
 
 /*
@@ -11,7 +12,17 @@ Solution 1 - (best practice)simply traverse
 Time complexity: O(N^2)
 Space complexity: O(1)
 */
-int * twoSum(int* nums, int numSize, int* target, int returnSize) {
+int * twoSum(int* nums, int numSize, int target, int* returnSize) {
+  for (int i = 0; i <= numSize; ++i) {
+    for (int j = i + 1; j <= numSize; ++j) {
+      if (num[i] + num[j] == target) {
+        int* ret = malloc(sizeof(int) * 2);
+        ret[0] = i, ret[1] = j;
+        *returnSize = 2; // why i need it ?
+        return ret;
+      }
+    }
+  }
 }
 
 
